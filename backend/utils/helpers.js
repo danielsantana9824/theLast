87eee10aa2266762ecf2f303273829af935836d6
John Ferlito <johnferlito73@gmail.com>
2025-01-21 19:31:30 -0500

    new

--- backend/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  generateOrderNumber: () => {
-    const prefix = 'ORD';
-    const timestamp = Date.now().toString().slice(-6);
-    const random = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
-    return `${prefix}-${timestamp}-${random}`;
-  }
-}; 
