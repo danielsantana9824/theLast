91caa0e60343aa5c3c0ce09a24cc912e6a908f9a
Marcelloro24 <mra24@me.com>
2025-01-27 19:38:38 -0500

    Added contact

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,6 +1,76 @@
-import { Box, Container, Heading, Text, VStack, FormControl, FormLabel, Input, Textarea, Button } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Box, Container, Heading, Text, VStack, FormControl, FormLabel, Input, Textarea, Button, Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  
+  const [showSuccess, setShowSuccess] = useState(false);
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // Reset form
+    setFormData({
+      name: '',
+      email: '',
+      message: ''
+    });
+
+    // Show success message
+    setShowSuccess(true);
+
+    // Redirect to home page after 3 seconds
+    setTimeout(() => {
+      navigate('/');
+    }, 3000);
+  };
+
+  if (showSuccess) {
+    return (
+      <Box 
+        height="calc(100vh - 64px)" 
+        display="flex" 
+        alignItems="center" 
+        justifyContent="center"
+      >
+        <Alert
+          status="success"
+          variant="subtle"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          height="200px"
+          bg="gray.800"
+          borderRadius="lg"
+          maxWidth="500px"
+        >
+          <AlertIcon boxSize="40px" mr={0} color="green.300" />
+          <AlertTitle mt={4} mb={1} fontSize="lg" color="white">
+            Message Sent!
+          </AlertTitle>
+          <AlertDescription maxWidth="sm" color="gray.300">
+            Thank you for contacting us, we will reply promptly
+          </AlertDescription>
+        </Alert>
+      </Box>
+    );
+  }
+
   return (
     <Container maxW="container.xl" py={8}>
       <VStack spacing={6} align="stretch">
@@ -12,12 +82,15 @@ function Contact() {
           Have questions? We'd love to hear from you. Send us a message and we'll respond as soon as possible.
         </Text>
 
-        <Box as="form" bg="gray.800" p={6} borderRadius="lg">
+        <Box as="form" bg="gray.800" p={6} borderRadius="lg" onSubmit={handleSubmit}>
           <VStack spacing={4}>
-            <FormControl>
+            <FormControl isRequired>
               <FormLabel color="gray.300">Name</FormLabel>
               <Input 
+                name="name"
                 type="text" 
+                value={formData.name}
+                onChange={handleInputChange}
                 bg="gray.700"
                 border="none"
                 color="white"
@@ -27,10 +100,13 @@ function Contact() {
               />
             </FormControl>
 
-            <FormControl>
+            <FormControl isRequired>
               <FormLabel color="gray.300">Email</FormLabel>
               <Input 
+                name="email"
                 type="email" 
+                value={formData.email}
+                onChange={handleInputChange}
                 bg="gray.700"
                 border="none"
                 color="white"
@@ -40,9 +116,12 @@ function Contact() {
               />
             </FormControl>
 
-            <FormControl>
+            <FormControl isRequired>
               <FormLabel color="gray.300">Message</FormLabel>
               <Textarea 
+                name="message"
+                value={formData.message}
+                onChange={handleInputChange}
                 bg="gray.700"
                 border="none"
                 color="white"
@@ -53,7 +132,12 @@ function Contact() {
               />
             </FormControl>
 
-            <Button colorScheme="blue" width="100%">
+            <Button 
+              type="submit" 
+              colorScheme="blue" 
+              width="100%"
+              isDisabled={!formData.name || !formData.email || !formData.message}
+            >
               Send Message
             </Button>
           </VStack>
